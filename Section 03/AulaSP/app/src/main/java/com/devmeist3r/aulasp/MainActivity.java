1d25f1cb9f71d1ff45d5a83940c91c73e0bdfd11
devmeist3r <luksinocencio@gmail.com>
2021-01-24 17:53:54 -0300

    *

--- Section 03/AulaSP/app/src/main/java/com/devmeist3r/aulasp/MainActivity.java
+++ Section 03/AulaSP/app/src/main/java/com/devmeist3r/aulasp/MainActivity.java
@@ -2,13 +2,26 @@ package com.devmeist3r.aulasp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private static final String TAG = "APP_AULA_SP";
+    private static final String PREF_NOME = "APP_AULA_SP_PREF";
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: Rodando");
+        sharedPreferences = getSharedPreferences(PREF_NOME, Context.MODE_PRIVATE);
+        Log.i(TAG, "onCreate: Pasta shared criada");
     }
+    
 }
