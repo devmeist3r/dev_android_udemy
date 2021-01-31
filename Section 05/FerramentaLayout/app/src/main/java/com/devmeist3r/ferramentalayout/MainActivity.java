0a0b346e79e30e54818bd5b6b222b0da99d3d492
devmeist3r <devmeist3r@gmail.com>
2021-01-31 19:34:34 -0300

    *

--- Section 05/FerramentaLayout/app/src/main/java/com/devmeist3r/ferramentalayout/MainActivity.java
+++ Section 05/FerramentaLayout/app/src/main/java/com/devmeist3r/ferramentalayout/MainActivity.java
@@ -3,12 +3,18 @@ package com.devmeist3r.ferramentalayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtTitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTitulo = findViewById(R.id.txtTitulo);
+        txtTitulo.setText("Retrato principal");
     }
 }
