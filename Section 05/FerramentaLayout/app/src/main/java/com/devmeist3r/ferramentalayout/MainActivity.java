bcbf093d6af470cbb85a185629179443cf162cac
devmeist3r <devmeist3r@gmail.com>
2021-01-31 22:14:40 -0300

    *

--- Section 05/FerramentaLayout/app/src/main/java/com/devmeist3r/ferramentalayout/MainActivity.java
+++ Section 05/FerramentaLayout/app/src/main/java/com/devmeist3r/ferramentalayout/MainActivity.java
@@ -7,14 +7,19 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView txtTitulo;
     EditText edtNomeCompleto;
     Button btnConfirmar;
+    ToggleButton tbLigado;
+    Switch swMostrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +30,11 @@ public class MainActivity extends AppCompatActivity {
         txtTitulo = findViewById(R.id.txtTitulo);
         edtNomeCompleto = findViewById(R.id.edtNomeCompleto);
         btnConfirmar = findViewById(R.id.btn_confirmar);
+        tbLigado = findViewById(R.id.tbLigado);
+        swMostrar = findViewById(R.id.swMostrar);
 
         // Acesso aos métodos dos componentes
-        txtTitulo.setText("Retrato principal");
+        txtTitulo.setText("Retrato");
 
         btnConfirmar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +51,30 @@ public class MainActivity extends AppCompatActivity {
                 // Toast
                 if (dadosOK) {
                     Toast.makeText(getBaseContext(), "Olá, " + edtNomeCompleto.getText(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        tbLigado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tbLigado.isChecked()) {
+                    edtNomeCompleto.setEnabled(false);
+                } else {
+                    edtNomeCompleto.setEnabled(true);
+                }
+            }
+        });
+
+        swMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (swMostrar.isChecked()) {
+                   txtTitulo.setVisibility(View.GONE);
+                } else {
+                    txtTitulo.setVisibility(View.VISIBLE);
+                    String novoTitulo = txtTitulo.getText().toString().toUpperCase();
+                    txtTitulo.setText(novoTitulo);
                 }
             }
         });
